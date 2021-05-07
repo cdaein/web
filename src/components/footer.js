95791c023089bd4ac9bfbab3725e860d6dcc3d8c
cdaein <cdaein@gmail.com>
2021-05-07 15:02:51 -0400

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <p>Copyright © 2019-2020 Dae In Chung</p>
+        <p>Copyright © 2019-2021 paperdove.com</p>
+        <p><a href="https://www.privacypolicygenerator.info/live.php?token=IuUXFahY27PLkblW7tjXFx52KRSrRX4Q">Privacy Policy</a></p>
         <p>This website is built with Gatsbyjs</p>
       </div>
     </footer>
