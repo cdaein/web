625be8ff4dbfd3a3316cc3859e5a3dd1404b8a6f
test <>
2022-01-03 13:36:11 -0500

    profile

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,19 @@
-import React from "react"
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <p>Copyright © 2019-2021 paperdove.com</p>
-        <p><a href="https://www.privacypolicygenerator.info/live.php?token=IuUXFahY27PLkblW7tjXFx52KRSrRX4Q">Privacy Policy</a></p>
+        <p>Copyright © 2019-2022 paperdove.com</p>
+        <p>
+          <a href="https://www.privacypolicygenerator.info/live.php?token=IuUXFahY27PLkblW7tjXFx52KRSrRX4Q">
+            Privacy Policy
+          </a>
+        </p>
         <p>This website is built with Gatsbyjs</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
