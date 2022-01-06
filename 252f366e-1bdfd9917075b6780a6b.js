9ca5ae48fdf2e8d7bbb5a9635dd26517794645f6
test <>
2022-01-06 11:26:23 -0500

    Updates

---
+++ 252f366e-1bdfd9917075b6780a6b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpaperdove=self.webpackChunkpaperdove||[]).push([[228],{2775:function(t,a,e){e.d(a,{KYK:function(){return r}});var n=e(1046);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-1bdfd9917075b6780a6b.js.map
