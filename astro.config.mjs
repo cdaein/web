d8df8f1ee740b59142958b6c55079d55fd84376d
user <3522003+cdaein@users.noreply.github.com>
2024-01-31 00:46:12 -0500

    fix: add astro-relative-links package
    hosted website doesn't load any asset (image, css, vid ..) so trying a package to solve.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,22 @@ import tailwind from "@astrojs/tailwind";
 // @ts-ignore
 // import rehypeFigure from "@microflash/rehype-figure";
 
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://paperdove.com",
-  integrations: [
-    mdx({
-      // extendMarkdownConfig: false,
-      // rehypePlugins: [rehypeFigure],
-    }),
-    tailwind({
-      // tailwind preflight
-      applyBaseStyles: true,
-      // Example: Allow writing nested CSS declarations
-      // alongside Tailwind's syntax
-      nesting: true,
-    }),
-  ],
+  integrations: [mdx({
+    // extendMarkdownConfig: false,
+    // rehypePlugins: [rehypeFigure],
+  }), tailwind({
+    // tailwind preflight
+    applyBaseStyles: true,
+    // Example: Allow writing nested CSS declarations
+    // alongside Tailwind's syntax
+    nesting: true
+  }), relativeLinks()],
   markdown: {
     // rehypePlugins: [rehypeFigure],
-  },
+  }
 });
