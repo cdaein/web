53602a5e3ed281a175e212df2b1ba4884aa873aa
mbp <3522003+cdaein@users.noreply.github.com>
2026-07-17 19:39:17 +0900

    fix: remove remnants from tailwind v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,33 +2,25 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
-// @ts-ignore
-// import rehypeFigure from "@microflash/rehype-figure";
-
-// import relativeLinks from "astro-relative-links";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://paperdove.com",
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss({
+        // NOTE: can i use these tw v3 astro plugin options below?
+        //   // tailwind preflight
+        //   applyBaseStyles: true,
+        //   // Example: Allow writing nested CSS declarations
+        //   // alongside Tailwind's syntax
+        //   nesting: true,
+      }),
+    ],
   },
   integrations: [
     mdx({
       // extendMarkdownConfig: false,
-      // rehypePlugins: [rehypeFigure],
     }),
-    // tailwind({
-    //   // tailwind preflight
-    //   applyBaseStyles: true,
-    //   // Example: Allow writing nested CSS declarations
-    //   // alongside Tailwind's syntax
-    //   nesting: true,
-    // }),
-    //
-    // relativeLinks(),
   ],
-  markdown: {
-    // rehypePlugins: [rehypeFigure],
-  },
+  markdown: {},
 });
